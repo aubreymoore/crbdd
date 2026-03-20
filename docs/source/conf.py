640c63b3754f0feedc8786962333e7a1c36b6e27
Aubrey Moore <Aubrey Moore>
2026-03-20 15:25:13 +0800

    imitial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from datetime import date
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'crbdd'
+copyright = f'{date.today().year}, Aubrey Moore'
+copyright = '2026, Aubrey Moore'
+author = 'Aubrey Moore'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'autoapi.extension',
+    'sphinx.ext.napoleon',   # Support for Google/NumPy docstrings
+    'sphinx.ext.viewcode',   # Links to source code
+]
+
+# AutoAPI settings: point to your src directory
+autoapi_dirs = ['../../src']
+autoapi_type = 'python'
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,11 @@
+# Welcome to crbdd
+
+This project documents development of tools for automated detection of coconut rhinoceros beetle damage in coconut palm tree images. 
+
+``{toctree}
+---
+caption: Contents
+maxdepth: 2
+project_setup
+autoapi/index
+---
